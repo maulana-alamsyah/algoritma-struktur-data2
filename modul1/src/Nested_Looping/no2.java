d79395412efc24cc4f75254394c618b6a66711b0
maulana-alamsyah <maulanalamsyah@gmail.com>
2021-03-15 19:10:42 +0700

    Tugas 1: Pengayaan Looping dan Array

--- modul1/src/Nested_Looping/no2.java
+++ modul1/src/Nested_Looping/no2.java
@@ -2,10 +2,9 @@ package Nested_Looping; //deklarasi package
 public class no2 { //bagian class
     public static void main(String[] args){ //method main
         int x,y;
-        
-        for(x=0; x<=4;x++){
-            for(y=0;y<x;y++){
-                System.out.print(y);
+        for (x=0; x <=4; x++){
+            for (y=0; y<x; y++){
+                System.out.print(x);
             }
             System.out.println();
         }
@@ -16,30 +15,30 @@ public class no2 { //bagian class
 
 //Penjelasan\\
 /*
-x=0; 0<=4; T; lanjut ke perulangan dalam
-y=0; 0<0; F; keluar dari perulangan dalam
+x=0; 0<=4; T; lanjut ke looping dalam
+y=0; 0<0; F; keluar dari looping dalam
 println()
-x++; x=0+1=1; 1<=4; T; lanjut ke perulangan dalam
-y=0; 0<1; T; print 0 ===========================================================> 0
-y++; y=0+1=1; 1<1; F keluar dari perulangan dalam
+x++; x=0+1=1; 1<=4; T; lanjut ke looping dalam
+y=0; 0<1; T; print 1 ===========================================================> 1
+y++; y=0+1=1; 1<1; F keluar dari looping dalam
 println()
-x++; x=1+1=2; 2<=4; T; lanjut ke perulangan dalam
-y=0; 0<2; T; print 0 ===========================================================>0
-y++; y=0+1=1; 1<2; T; print 1 ==================================================>01
-y++; y=1+1=2; 2<2; F; keluar dari perulangan dalam
+x++; x=1+1=2; 2<=4; T; lanjut ke looping dalam
+y=0; 0<2; T; print 2 ===========================================================>2
+y++; y=0+1=1; 1<2; T; print 2 ==================================================>22
+y++; y=1+1=2; 2<2; F; keluar dari looping dalam
 println()
-x++; x=2+1=3; 3<=4; T; lanjut ke perulangan dalam
-y=0; 0<3; T; print 0 ===========================================================>0
-y++; y=0+1=1; 1<3; T; print 1 ==================================================>01
-y++; y=1+1=2; 2<3; T; print 2 ==================================================>012
-y++; y=2+1=3; 3<3; F; keluar dari perulangan dalam
+x++; x=2+1=3; 3<=4; T; lanjut ke looping dalam
+y=0; 0<3; T; print 3 ===========================================================>3
+y++; y=0+1=1; 1<3; T; print 3 ==================================================>33
+y++; y=1+1=2; 2<3; T; print 3 ==================================================>333
+y++; y=2+1=3; 3<3; F; keluar dari looping dalam
 println()
-x++; x=3+1=4; 4<=4; T; lanjut ke perulangan dalam
-y=0; 0<4; T; print 0 ===========================================================>0
-y++; y=0+1=1; 1<4; T; print 1 ==================================================>01
-y++; y=1+1=2; 2<4; T; print 2 ==================================================>012
-y++; y=2+1=3; 3<4; T; print 3 ==================================================>0123
-y++; y=3+1=4; 4<4; F; keluar dari perulangan dalam
+x++; x=3+1=4; 4<=4; T; lanjut ke looping dalam
+y=0; 0<4; T; print 4 ===========================================================>4
+y++; y=0+1=1; 1<4; T; print 4 ==================================================>44
+y++; y=1+1=2; 2<4; T; print 4 ==================================================>444
+y++; y=2+1=3; 3<4; T; print 4 ==================================================>4444
+y++; y=3+1=4; 4<4; F; keluar dari looping dalam
 println()
-x++; x=4+1=5; 5<=4->F; keluar dari perulangan luar
+x++; x=4+1=5; 5<=4->F; keluar dari looping luar
 */
--- modul1/src/arrayPerulangan_3.java
+++ modul1/src/arrayPerulangan_3.java
@@ -1,16 +1,15 @@
 public class arrayPerulangan_3 { //bagian class
     public static void main(String args[]){ //method main
-        String[] siswa = {"Reinan", "Odena", "Geanno"};//panjang array 3 (documentation section
-        
-        for(int i = 0; i<siswa.length;i++){
-            System.out.println("Indeks ke " + i + " adalah " + siswa[i]);
-        } 
+        String[] siswa = {"Reinan", "Odena", "Geanno"}; //panjang array 3
+        for (int i=0; i < siswa.length; i++){
+            System.out.println("Indeks ke " + i + " = " + siswa[i]);
+        }
     }
 }
 //Penjelasan\\
 /*
-i=0; 0<3 -> T; print "Indeks ke " + 0 + " adalah " + siswa[0] ==================>Indeks ke 0 adalah Reinan
-i++; i=0+1=1; 1<3 -> T; print "Indeks ke " + 1 + " adalah " + siswa[1] =========>Indeks ke 1 adalah Odena
-i++; i=1+1=2; 2<3 -> T; print "Indeks ke " + 2 + " adalah " + siswa[2] =========>Indeks ke 2 adalah Geanno
+i=0; 0<3 -> T; print "Indeks ke " + 0 + " = " + siswa[0] ==================>Indeks ke 0 = Reinan
+i++; i=0+1=1; 1<3 -> T; print "Indeks ke " + 1 + " = " + siswa[1] =========>Indeks ke 1 = Odena
+i++; i=1+1=2; 2<3 -> T; print "Indeks ke " + 2 + " = " + siswa[2] =========>Indeks ke 2 = Geanno
 i++; i=2+1=3; 3<3 -> F; keluar dari perulangan
 */
